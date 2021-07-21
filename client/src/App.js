91f2aff782ee9840e18a5716272fd124a564177c
C4 Patino <c4patino@gmail.com>
2021-07-21 17:58:16 -0500

    Removed unnecesary files and added router with components

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            {/* <Topbar /> or layout element of some kind */}
+            <Switch>
+                <Route path='/'>
+                    <Home />
+                </Route>
+                <Route path='/login'>{/* <Login /> */}</Route>
+                <Route path='/register'>{/* <Register /> */}</Route>
+                <Route path='/pack'>{/* <Pack /> */}</Route>
+                <Route path='/bookmarks'>{/* <Bookmarks /> */}</Route>
+                <Route path='/saved'>{/* <Saved /> */}</Route>
+                <Route path='/recover-password'>{/* <Recovery /> */}</Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
