1f9c5372725c01d033f9f4f17676ef2f9fee241b
C4 Patino <c4patino@gmail.com>
2021-07-21 23:07:10 -0500

    Modified routes to work with new models

--- backend/routes/auth/GenerateToken.js
+++ backend/routes/auth/GenerateToken.js
@@ -1,16 +1,33 @@
 const { sha512 } = require('crypto-hash');
+const { v4: uuidv4 } = require('uuid');
 
 const Tokens = require('../../models/Token');
 
 async function GenerateToken(req, res, next) {
-    const token = await sha512(`${new Date()}${req.username}`); // Someone come up with something else to hash, this is probably not a good idea
-    const username = req.body.username;
+    const token = await sha512(`${uuidv4()}${req.username}`);
+    const user = req.user;
+    const remember = req.body.remember;
+    const expires = remember ? null : Date.now() + 3600000;
 
     // Delete previous/timed-out tokens and create a new one for that username
-    await Tokens.deleteMany({ username });
-    Tokens.create({ username, token });
+    await Tokens.deleteMany({ username: user.username });
 
-    res.send(token);
+    try {
+        const result = await Tokens.create({
+            username: user.username,
+            token,
+            expires,
+        });
+
+        res.send({
+            token: result.token,
+            expires: result.expires,
+        });
+    } catch (e) {
+        console.log(e);
+
+        res.status(500).send('Whoops, something went wrong.');
+    }
 }
 
 module.exports = GenerateToken;
