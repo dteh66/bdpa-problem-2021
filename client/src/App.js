8fcdb2ddabfb18260d309d8fc1043a652d182a40
C4 Patino <c4patino@gmail.com>
2021-07-21 20:31:59 -0500

    Created register route for frontend and added to router

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,22 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
+import Register from './components/Register';
+import Login from './components/Login';
 
 function App() {
     return (
         <Router>
             {/* <Topbar /> or layout element of some kind */}
             <Switch>
-                <Route path='/'>
+                <Route path='/' exact>
                     <Home />
                 </Route>
-                <Route path='/login'>{/* <Login /> */}</Route>
-                <Route path='/register'>{/* <Register /> */}</Route>
+                <Route path='/login'>
+                    <Login />
+                </Route>
+                <Route path='/register'>
+                    <Register />
+                </Route>
                 <Route path='/pack'>{/* <Pack /> */}</Route>
                 <Route path='/bookmarks'>{/* <Bookmarks /> */}</Route>
                 <Route path='/saved'>{/* <Saved /> */}</Route>
