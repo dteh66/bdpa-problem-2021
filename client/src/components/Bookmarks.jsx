d095c5250ba6ef3e73a33492d5b675cd1496afe4
Test <cnoland1011@gmail.com>
2021-07-23 17:08:11 -0500

    I created a bookmarks.jsx file and added the route

---
+++ client/src/components/Bookmarks.jsx
@@ -0,0 +1,18 @@
+import { makeStyles, Typography } from '@material-ui/core'
+
+const useStyles = makeStyles({
+    heading: {
+        fontSize: '30px',
+        margin: 'auto'
+    }
+})
+
+const Bookmark = () => {
+    const classes = useStyles()
+
+    return (
+        <Typography className={classes.heading}>Your Bookmarked Barks</Typography>
+    )
+}
+
+export default Bookmark
