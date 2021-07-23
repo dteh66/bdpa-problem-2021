771c54f9c3dfa49541b35d3be462d2bf5e112588
C4 Patino <c4patino@gmail.com>
2021-07-22 20:35:12 -0500

    Fixed token 404 on delete route

--- backend/middleware/tokenAuth.js
+++ backend/middleware/tokenAuth.js
@@ -16,7 +16,7 @@ async function tokenAuth(req, res, next) {
 
     // Check whether token is expired or if there was no result
     if (!result || (result.expired !== null && new Date() < result.expired)) {
-        await Tokens.deleteMany({ login });
+        await Tokens.deleteMany({ username });
         res.status(511);
         next(new Error('Session Expired'));
         return;
