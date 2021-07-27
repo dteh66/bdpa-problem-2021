17ed61ae5daaf6befe9746217c895c49d4a7426a
C4 Patino <c4patino@gmail.com>
2021-07-27 09:03:57 -0500

    Added theoretical method for sorting

--- backend/routes/index/GetBarks.js
+++ backend/routes/index/GetBarks.js
@@ -1,4 +1,16 @@
 const Barks = require('../../models/Bark');
+const Tokens = require('../../models/Token');
+const Users = require('../../models/User');
+
+/* SORTING ALGORITHM */
+// Filter for followed (AUTHED ONLY)
+// 1. Destructure the created field and to a $group on years.
+// 2. Then do a $group on months.
+// 3. Then do a $group on days.
+// 4. Then, you could do a $group on individual hours.
+// 5. Do a sort on all groups using likes as the reference.
+// 6. Recombine all groups.
+// 7. (AUTHED ONLY) Append separate query sorted by 1-6 from non-followed users.
 
 function GetBarks(req, res, next) {
     const token = req.query.token;
