f805441b4feed36fec5b1244e34e1e67eb1e1b91
C4 Patino <c4patino@gmail.com>
2021-07-27 08:35:19 -0500

    Added logic for separate return if token is provided

--- backend/routes/index/GetBarks.js
+++ backend/routes/index/GetBarks.js
@@ -1,5 +1,21 @@
 const Barks = require('../../models/Bark');
 
-function GetBarks(req, res, next) {}
+function GetBarks(req, res, next) {
+    const token = req.query.token;
+    if (token) {
+        // Find the user if the token is sent in a query
+        const result = await Tokens.findOne({ token });
+        const userId = result ? result.user : null;
+
+        if (
+            !result ||
+            (result.expired !== null && new Date() < result.expired)
+        ) {
+            await Tokens.deleteMany({ user: userId });
+            return res.status(511).send('Session Expired');
+        }
+        const user = await Users.findById(userId);
+    }
+}
 
 module.exports = GetBarks;
